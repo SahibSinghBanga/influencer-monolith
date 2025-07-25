e6281400c4854c5c2842b694b8fe605d9c6b5a98
sahibsingh27aug <sahibsinghbanga1@gmail.com>
2025-07-26 01:05:46 +0530

    Transfer Auth Logic to User MS

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,26 +32,13 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        if (!\Auth::attempt($request->only('email', 'password'))) {
-            return response([
-                'error' => 'invalid credentials'
-            ], Response::HTTP_UNAUTHORIZED);
-        }
+        $scope = $request->path() === 'api/admin/login' ? 'admin' : 'ambassador';
 
-        $user = \Auth::user();
+        $data = $request->only('email', 'password') + compact('scope');
 
-        $adminLogin = $request->path() === 'api/admin/login';
+        $response = $this->userService->post('login', $data);
 
-        if ($adminLogin && !$user->is_admin) {
-            return response([
-                'error' => 'Access Denied!'
-            ], Response::HTTP_UNAUTHORIZED);
-        }
-
-        $scope = $adminLogin ? 'admin' : 'ambassador';
-        $jwt = $user->createToken('token', [$scope])->plainTextToken;
-
-        $cookie = cookie('jwt', $jwt, 60 * 24); // 1 day
+        $cookie = cookie('jwt', $response['jwt'], 60 * 24); // 1 day
 
         return response([
             'message' => 'success'
@@ -60,9 +47,7 @@ class AuthController extends Controller
 
     public function user(Request $request)
     {
-        $user = $request->user();
-
-        return new UserResource($user);
+        return $this->userService->get('user');
     }
 
     public function logout()
--- app/Services/ApiService.php
+++ app/Services/ApiService.php
@@ -12,4 +12,11 @@ abstract class ApiService
     {
         return Http::post("{$this->endpoint}/{$path}", $data)->json();
     }
+
+    public function get($path)
+    {
+        return Http::acceptJson()->withHeaders([
+            'Authorization' => 'Bearer ' . request()->cookie('jwt')
+        ])->get("{$this->endpoint}/{$path}")->json();
+    }
 }
